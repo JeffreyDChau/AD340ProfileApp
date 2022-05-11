514dd0654a53894414c658abaecebb17b1fda0b3
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-05-10 23:35:04 -0700

    like test

--- app/src/androidTest/java/com/example/ad340profile/WelcomeScreenTest.java
+++ app/src/androidTest/java/com/example/ad340profile/WelcomeScreenTest.java
@@ -28,6 +28,12 @@ public class WelcomeScreenTest {
         onView(withId(R.id.matches_menu_item)).perform(click());
 
         onView(withRecyclerView(R.id.recycler_view).atPosition(0))
-                .check(matches(hasDescendant(withText("Mark T"))));
+                .check(matches(hasDescendant(withText("Mark Palpatine"))));
+        onView(withRecyclerView(R.id.recycler_view).atPosition(1))
+                .check(matches(hasDescendant(withText("Grumpy cat"))));
+        onView(withRecyclerView(R.id.recycler_view).atPosition(2))
+                .check(matches(hasDescendant(withText("Steve"))));
+        onView(withRecyclerView(R.id.recycler_view).atPosition(3))
+                .check(matches(hasDescendant(withText("Random Duck"))));
     }
 }
