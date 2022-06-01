fb4ef236b5179fe2d4e454552ae8d30b451a3a9d
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-05-31 21:00:04 -0700

    Location services

--- app/src/main/java/com/example/ad340profile/SettingsFragment.java
+++ app/src/main/java/com/example/ad340profile/SettingsFragment.java
@@ -1,13 +1,5 @@
 package com.example.ad340profile;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 
